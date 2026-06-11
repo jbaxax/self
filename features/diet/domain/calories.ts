64526cd91be1008dabbaac07f0fab9584ea7b345
walter834 <wjave@hsperu.pe>
2026-06-11 12:13:55 -0500

    feat(diet): define CalorieInput type with necessary fields for calorie tracking

---
+++ features/diet/domain/calories.ts
@@ -0,0 +1,10 @@
+import { Enums } from "@/lib/supabase/types";
+
+export type CalorieInput = {
+    weight: number;
+    height: number;
+    age: number;
+    sex: Enums<"sex_type">
+    activityLevel: Enums<"activity_level_type">;
+    goal: Enums<"goal_type">;
+}
